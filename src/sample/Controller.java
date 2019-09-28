724d316d08e6998f6cd54e2be06f8f17cf5c5d0e
Sabrina Kienholz <snkienholz2938@eagle.fgcu.edu>
2019-09-28 16:53:48 -0400

    Completed tasks for Week 1-3: Basic framework for the application and CSS styling.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,43 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+  @FXML
+  private Tab productLine;
+
+  @FXML
+  private GridPane productLineInfo;
+
+  @FXML
+  private Button btnAddProduct;
+
+  @FXML
+  private AnchorPane tblExistingProducts;
+
+  @FXML
+  private Button btnRecordProduction;
+
+  @FXML
+  private Tab produce;
+
+  @FXML
+  private Tab productionLog;
+
+  @FXML
+  void addProduct(ActionEvent event) {
+    System.out.println("hi");
+  }
+
+  @FXML
+  void recordProduction(ActionEvent event) {
+    System.out.println("bon giorno");
+  }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("productLine.fxml"));
+    primaryStage.setTitle("Production Line Tracker");
+    Scene scene = new Scene(root, 750, 600);
+    primaryStage.setScene(scene);
+    scene.getStylesheets().add(Main.class.getResource("WindowStyle.css").toExternalForm());
+    primaryStage.show();
+  }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }

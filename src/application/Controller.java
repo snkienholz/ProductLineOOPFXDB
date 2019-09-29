1041630c6d1808cc1aaa0d3f22b775c86f8273fb
Sabrina Kienholz <snkienholz2938@eagle.fgcu.edu>
2019-09-28 23:53:24 -0400

    Completed Week 5 tasks | Sprint 1 completed

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,28 +1,76 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
+/**
+ * Full application controller.
+ *
+ * @author Sabrina
+ */
 public class Controller {
 
-  @FXML
-  private Tab productLine;
+  private Connection conn = null;
+  private Statement stmt = null;
+
+  /**
+   * Initializing connection to database.
+   */
+  public void initialize() {
+
+    final String jdbcDriver = "org.h2.Driver";
+    final String dbUrl = "jdbc:h2:./res/Product";
+
+    //  Database credentials
+    final String user = "";
+    final String pass = "";
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(jdbcDriver);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(dbUrl, user, pass);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
+
+    // ComboBox
+    for (int i = 1; i <= 10; i++) {
+      cboQuantity.getItems().add(i);
+    }
+    cboQuantity.setEditable(true);
+    cboQuantity.getSelectionModel().selectFirst();
+  }
 
   @FXML
   private GridPane productLineInfo;
 
   @FXML
-  private Button btnAddProduct;
-
-  @FXML
   private AnchorPane tblExistingProducts;
 
   @FXML
+  private Button btnAddProduct;
+
+  @FXML
   private Button btnRecordProduction;
+
+  @FXML
+  private Tab productLine;
 
   @FXML
   private Tab produce;
@@ -30,13 +78,39 @@ public class Controller {
   @FXML
   private Tab productionLog;
 
+  /**
+   * Product Line text fields.
+   */
+  @FXML
+  private TextField txtProductName;
+
+  @FXML
+  private TextField txtManufacturer;
+
+  @FXML
+  private TextField txtChoiceBox;
+
+  @FXML
+  private ComboBox<Integer> cboQuantity;
+
+  /**
+   * Allows user to add products and stores them in the database.
+   *
+   * @param event Event handler from Scene Builder.
+   */
   @FXML
   void addProduct(ActionEvent event) {
-    System.out.println("hi");
+    String sql = "INSERT INTO Product(type, manufacturer, name) "
+        + "VALUES ('Audio', 'Apple', 'iPhone 11 Pro Max')";
+    try {
+      stmt.executeUpdate(sql);
+    } catch (SQLException e) {
+      // handle errors for JDBC
+      e.printStackTrace();
+    }
   }
 
   @FXML
   void recordProduction(ActionEvent event) {
-    System.out.println("bon giorno");
   }
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,16 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main application file. Sets up scene and launches FX application.
+ *
+ * @author Sabrina
+ */
 public class Main extends Application {
 
   @Override
@@ -24,38 +25,5 @@ public class Main extends Application {
 
   public static void main(String[] args) {
     launch(args);
-
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./res/Product";
-
-    //  Database credentials
-    final String USER = "";
-    final String PASS = "";
-    Connection conn = null;
-    Statement stmt = null;
-
-    try {
-      // STEP 1: Register JDBC driver
-      Class.forName(JDBC_DRIVER);
-
-      //STEP 2: Open a connection
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-      //STEP 3: Execute a query
-      /*stmt = conn.createStatement();
-
-      String sql = "SELECT * FROM JOBS";
-
-      ResultSet rs = stmt.executeQuery(sql);
-      while (rs.next()) {
-        System.out.println(rs.getString(1));
-      } */
-
-      // STEP 4: Clean-up environment
-      //stmt.close();
-      conn.close();
-    } catch (ClassNotFoundException | SQLException e) {
-      e.printStackTrace();
-    }
   }
 }
